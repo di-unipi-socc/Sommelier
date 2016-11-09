e78dd53e84a9fce8067716a388a884053fe06816
aditommaso <ditommaso.dt@gmail.com>
2016-11-09 16:46:44 +0100

    improved visualisation of results

--- topologyvalidator.py
+++ topologyvalidator.py
@@ -292,23 +292,23 @@ class TopologyValidator():
 
 		codeError = errorList[0]
 		if codeError == 1.1:
-			print '1.1 - MISSING_REQUIREMENT_DEFINITION: The requirement is assigned but not defined.\n'
+			print '1.1 - MISSING_REQUIREMENT_DEFINITION: The requirement is assigned but not defined.'
 		elif codeError == 1.2:
-			print '1.2 - NODE_TYPE_NOT_COHERENT: The type "%s" of the target node "%s" is not valid (as it differs from that indicated in the requirement definition).' % (errorList[1], errorList[2]), '\n'
+			print '1.2 - NODE_TYPE_NOT_COHERENT: The type "%s" of the target node "%s" is not valid (as it differs from that indicated in the requirement definition).' % (errorList[1], errorList[2])
 		elif codeError == 1.3:
-			print '1.3 - CAPABILITY_TYPE_NOT_COHERENT: The type of the target capability is not valid (as it differs from that indicated in the requirement definition).\n'
+			print '1.3 - CAPABILITY_TYPE_NOT_COHERENT: The type of the target capability is not valid (as it differs from that indicated in the requirement definition).'
 		elif codeError == 1.4:
-			print '1.4 - MISSING_CAPABILITY_ERROR: The target node template "%s" is not offering any capability whose type is compatible with "%s" (indicated in the requirement definition).' % (errorList[1], errorList[2]), '\n'
+			print '1.4 - MISSING_CAPABILITY_ERROR: The target node template "%s" is not offering any capability whose type is compatible with "%s" (indicated in the requirement definition).' % (errorList[1], errorList[2])
 		elif codeError == 1.5:
-			print '1.5 - RELATIONSHIP_TYPE_NOT_COHERENT: The type of the outgoing relationship is not valid (as it differs from that indicated in the requirement definition).\n'
+			print '1.5 - RELATIONSHIP_TYPE_NOT_COHERENT: The type of the outgoing relationship is not valid (as it differs from that indicated in the requirement definition).'
 		elif codeError == 2.1:
-			print '2.1 - CAPABILITY_VALID_TARGET_TYPE_ NOT_COHERENT: The type of the target capability "%s" is not valid (as it differs from that indicated in the definition of the type of the outgoing relationship).' %(errorList[1]), '\n'
+			print '2.1 - CAPABILITY_VALID_TARGET_TYPE_ NOT_COHERENT: The type of the target capability "%s" is not valid (as it differs from that indicated in the definition of the type of the outgoing relationship).' %(errorList[1])
 		elif codeError == 2.2:
-			print '2.2 - MISSING_CAPABILITY_VALID_TARGET_TYPE: The target node template "%s" is not offering any capability whose type is compatible with those indicated as valid targets for the type of the outgoing relationship.' % (errorList[1]), '\n'
+			print '2.2 - MISSING_CAPABILITY_VALID_TARGET_TYPE: The target node template "%s" is not offering any capability whose type is compatible with those indicated as valid targets for the type of the outgoing relationship.' % (errorList[1])
 		elif codeError == 3.1:
-			print '3.1 - CAPABILITY_VALID_SOURCE_TYPE_ NOT_COHERENT: The node type "%s" is not a valid source type for the capability targeted by the outgoing relationship (as it differs from those indicated in the capability type).' % (errorList[1]), '\n'				 
+			print '3.1 - CAPABILITY_VALID_SOURCE_TYPE_ NOT_COHERENT: The node type "%s" is not a valid source type for the capability targeted by the outgoing relationship (as it differs from those indicated in the capability type).' % (errorList[1])				 
 		elif codeError == 3.2:
-			print '3.2 - CAPABILITY_DEFINITION_VALID_SOURCE_TYPE_NOT_COHERENT: The node type "%s" is not a valid source type for the capability targeted by the outgoing relationship (as it differs from those indicated in the capability definitions in the type of "%s").' % (errorList[1], errorList[2]), '\n'				 
+			print '3.2 - CAPABILITY_DEFINITION_VALID_SOURCE_TYPE_NOT_COHERENT: The node type "%s" is not a valid source type for the capability targeted by the outgoing relationship (as it differs from those indicated in the capability definitions in the type of "%s").' % (errorList[1], errorList[2])				 
 
 	# Prints the result of the validation
 	def printValidation(self, validation):
@@ -317,10 +317,11 @@ class TopologyValidator():
 			reqs = validation.get(nodeName).keys()
 			for req in reqs:
 				infoList = validation.get(nodeName).get(req)
-				for info in infoList:
-					isCorrect = False
+				if infoList != []:
 					print "\nNODE TEMPLATE: ",nodeName
 					print "REQUIREMENT: ",req
+				for info in infoList:
+					isCorrect = False
 					self.printError(info)
 		if isCorrect:
 			print "The application topology is valid."
